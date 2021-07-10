699d7bea3da2cfd2d2ee5d08266cdd76fe543b94
chris <git@cweyermann.de>
2021-07-10 21:12:39 +0200

    initial commit

---
+++ src/main/java/de/cweyermann/sspes/api/SayHello.java
@@ -0,0 +1,11 @@
+package de.cweyermann.sspes.api;
+
+public class SayHello {
+
+    public String sayIt() {
+        String hello = "HELLO! Goddamn it";
+        System.out.println(hello);
+
+        return hello;
+    }
+}
